1ad236763f3ef4baf5d3e63f2edcfba1c4093e87
April Walker <aprilcotmail@gmail.com>
2019-10-08 16:45:22 -0500

    phase 2ish

--- HW2/disambiguation.py
+++ HW2/disambiguation.py
@@ -8,6 +8,8 @@
 # Author: April Walker
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 import numpy as np
+import pandas as pd
+import ipdb
 import math
 import time
 import nltk
@@ -17,6 +19,20 @@ stop_words = set(stopwords.words('english'))
 
 file = open('amazon_reviews.txt', 'r', encoding='utf-8')
 raw = file.read()
+
+#
+# save_to_file: simple function saves array of text to file with new line
+#   between elements, snagged from interwebs and changed a tad
+
+#
+#   input--
+#   filename: contains output filename/path (str)
+#   text: array of text to write to file (array)
+def save_to_file(filename, text):
+    with open(filename, mode='wt', encoding='utf-8') as myfile:
+        for line in text:
+            myfile.write(line)
+            myfile.write('\n')
 
 #
 # clean_corpus: minimally preprocesses corpus per nltk parser given by S. Gauch
@@ -37,8 +53,16 @@ def clean_corpus(corpus):
     # remove words only occuring once
     words = [word for word in words if freq[word] > 1]
     return words
-
+#
+# wordpair_context: grabs conext and saves train/test from corpus for wordpairs
+#   input--
+#   wordpairs: contains all words for which context is wanted (array of tuples)
+#   words: an array of strings conatining the words in the corpus (list)
+#   n: gives the width for which context is wanted (int)
 def wordpair_context(wordpairs, words, n):
+    # get context: internal function to get context for specific word
+    # inputs: my_word (str), words (array), n (int)
+    # outputs: context (array)
     def get_context(my_word, words, n):
         # note word is skipped if appropropriate context not avaliable
         context = []
@@ -46,30 +70,25 @@ def wordpair_context(wordpairs, words, n):
             if words[i] == my_word:
                 # get context for instance of word
                 tmp = words[i-n:i+(n+1)]
-                tmp.pop(i)
+                tmp.pop(n)
+                # append context for this instance to array
                 context.append(' '.join(tmp))
         return context
 
     for pair in wordpairs:
         for my_word in pair:
             # declare filenames
-            train_file = '_'.join(my_word, "training.txt")
-            test_file = '_'.join(my_word, "testing.txt")
+            train_file = '_'.join([my_word, "training.txt"])
+            test_file = '_'.join([my_word, "testing.txt"])
             # get context
             context = get_context(my_word, words, n)
             # train/test split
             msk = np.random.rand(len(context)) < 0.8
-            train = context[msk]
-            test = context[~msk]
+            train = np.array(context)[msk]
+            test = np.array(context)[~msk]
             # save train/test sets
-            fs.writeFile(train_file,
-                train.join('\n'),
-                function (err) { console.log(''.join('Training data saved for', my_word)); }
-            );
-            fs.writeFile(test_file,
-                test.join('\n'),
-                function (err) { console.log(''.join('Testing data saved for', my_word)); }
-            );
+            save_to_file(train_file, train)
+            save_to_file(test_file, test)
 
 
 
@@ -87,7 +106,7 @@ words = clean_corpus(raw)
 end = time.time()
 print((end - start), 'seconds elapsed')
 
-# # # Phase 1: Extract and save wordpairs
+# # # Step 1: Extract and save wordpairs
 
 # indicate wordpairs of interest here
 wordpairs = [("night", "seat"),
@@ -98,19 +117,49 @@ wordpairs = [("night", "seat"),
              ("huge", "heavy")]
 
 # extract and save (only call this once...)
-wordpair_extraction(wordpairs, words)
+wordpair_context(wordpairs, words, 10)
 
-# # # Phase 2: Create pseudowords (truth file creation)
+for pair in wordpairs:
+    # # #
+    # Step 2: Prep out train/test datasets
+    # # #
+    train = pd.DataFrame()
+    test = pd.DataFrame()
+    truth = pd.DataFrame() # a copy of the test dataset with the true target
+    for word in pair:
+        # declare filenames to grab context
+        train_file = '_'.join([word, "training.txt"])
+        test_file = '_'.join([word, "testing.txt"])
+        # get context
+        new_train = pd.read_csv(train_file, delimiter=' ', header=None)
+        new_test = pd.read_csv(test_file, delimiter=' ', header=None)
+        # append true target value
+        new_train["target"] = word
+        new_test["target"] = word
+        # append dataframes to train/truth sets
+        train = train.append(new_train)
+        truth = truth.append(new_test)
+    # shuffle our dataframes once both wordpairs are present
+    train = train.sample(frac=1)
+    truth = truth.sample(frac=1)
+    # drop our known target for our test set
+    test = truth.drop(["target"], axis=1)
 
-# TODO: for each word, replace word in corpus with pseudoword and create truth
-# file of which was the original word. Do at most one replacment per line for
-# simplicity... make note of the line number and original word in truth file
+    # # #
+    # Step 3: Training our classifiers. On top our standard naive bayes I'm
+    #   going to try out some wild card ML classifiers. Because I expect these
+    #   to take longer, I'll only test them out on a few instances of my
+    #   word pairs.
+    # # #
+    wildcard_wordpairs = [("car", "bike"),
+                          ("big", "small"),
+                          ("huge", "heavy")]
 
-# # #  Phase 3: Training our classifiers
 
-# # # Phase 4: Testing our classifiers
+    # While unecessary, I will save the classifer output to a txt for
+    # curiosities sake... It might be neat to see *where* my classifiers
+    # performed bad/good.
 
-# Phase 5 and 6 discussed in paper!
 
 # # # Bonus Section:
 
